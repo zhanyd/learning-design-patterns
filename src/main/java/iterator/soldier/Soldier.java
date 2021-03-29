7b5c3c0e9cbc9275d064fd54dd08231a60c59b2e
zhanyd <zhan@zhanyddeMacBook-Pro.local>
2021-03-29 22:01:34 +0800

    add

---
+++ src/main/java/iterator/soldier/Soldier.java
@@ -0,0 +1,47 @@
+package iterator.soldier;
+
+/**
+ * 士兵类
+ */
+public class Soldier {
+    /**
+     * 姓名
+     */
+    String name;
+    /**
+     * 兵种
+     */
+    String unit;
+    /**
+     * 所属
+     */
+    String belongs;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getBelongs() {
+        return belongs;
+    }
+
+    public void setBelongs(String belongs) {
+        this.belongs = belongs;
+    }
+
+
+
+
+}
---
+++ src/main/java/iterator/soldier/ZhouCangArrayArmy.java
@@ -0,0 +1,7 @@
+package iterator.soldier;
+
+public class ZhouCangArrayArmy {
+
+    Soldier[] soldiers = new Soldier[5];
+
+}
