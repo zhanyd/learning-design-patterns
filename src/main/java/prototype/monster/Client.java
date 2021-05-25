47e171c9ab308e8e475a1a94a29fa82a69d6185f
zhanyd <zhan@zhanyddeMacBook-Pro.local>
2021-05-25 21:35:59 +0800

    add

---
+++ src/main/java/prototype/monster/Client.java
@@ -0,0 +1,18 @@
+package prototype.monster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        List<Monster> monsterList = new ArrayList<Monster>();
+        Monster monster = new Monster("飞龙", 200, 100);
+
+        for(int i = 0; i < 10; i++) {
+            monsterList.add((Monster)monster.clone());
+        }
+
+        monsterList.stream().forEach(f -> System.out.println(f));
+    }
+}
---
+++ src/main/java/prototype/monster/Monster.java
@@ -0,0 +1,63 @@
+package prototype.monster;
+
+/**
+ * 怪物类
+ */
+public class Monster implements Cloneable{
+
+    /**
+     * 名称
+     */
+    String name;
+
+    /**
+     * 攻击力
+     */
+    int attackPower;
+
+    /**
+     * 生命值
+     */
+    int hp;
+
+    public Monster(String name, int attackPower, int hp) {
+        this.name = name;
+        this.attackPower = attackPower;
+        this.hp = hp;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "怪物名称：" + name + "，攻击力：" + attackPower + "，生命值：" + hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public void setAttackPower(int attackPower) {
+        this.attackPower = attackPower;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+}
